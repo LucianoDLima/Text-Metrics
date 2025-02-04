5f7a7f3ef9db9fc13dffff82c3099ac502a16667
Luciano Lima <ljr.luciano.junior@outlook.com>
2025-02-03 21:52:05 -0300

    feat: Add hero section

--- src/app/layout/ui/layout.tsx
+++ src/app/layout/ui/layout.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@widgets/header';
 import './layout.scss';
+import { Hero } from '@widgets/hero';
 
 export function Layout() {
   return (
     <div className='wrapper'>
       <Header />
+      <Hero />
     </div>
   );
 }
